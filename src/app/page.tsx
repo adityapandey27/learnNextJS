88e039fbb71000d67cd5ee8f209f04450662cc64
adityapandey27 <adityapandey272002@gmail.com>
2025-09-10 01:35:18 +0530

    link which is linking pages with each other

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
+
 export default function page(){
     return (
-        <>First route</>
+        <>First route
+        <Link href={"./about"}>Click here</Link>
+        </>
     )
 } 
