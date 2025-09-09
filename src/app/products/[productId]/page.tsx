5c5464ed9b1d0869cd7d6c69b68b8bac51cbf23a
adityapandey27 <adityapandey272002@gmail.com>
2025-09-10 01:01:52 +0530

    meta data

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,19 @@
+import { Metadata } from "next";
+
+type props = {
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetaData=async ({params}:props):Promise<Metadata>=>{
+  const id=(await params).productId;
+  return {
+    title:`Product ${id}`
+  }
+}
+
 export default async function ProductDetails({
   params,
-}: {
-  params: { productId: string };
-}) {
+}: props) {
   const { productId } = await params;
   return <>First Page route : {productId}</>;
 }
